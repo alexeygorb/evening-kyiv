3ad4fc72bc3b951c9516ca68eab4968b6fc1495a
Alexey Gorb <xalgo@berlingskemedia.dk>
2014-11-12 23:43:36 +0200

    News page finilized

--- layout/js/scripts.js
+++ layout/js/scripts.js
@@ -2,6 +2,7 @@ $(function() {
 
   $('.news-latest-3,.news-latest-4').equalizer({columns: 'article'});
   $('.events-list').equalizer({columns: 'article'});
+  $('.read-more-list').equalizer({columns: 'article'});
   $('.headlines .headline-row').equalizer({columns: '.headline'});
   //$('.content-wrap:eq(1)').equalizer({ columns : '> div', max: 165, overflow : 'stinky' });
   //$('.content-wrap:eq(2)').equalizer({ min: 140 });
--- layout/templates/content-news.tpl.php
+++ layout/templates/content-news.tpl.php
@@ -1,7 +1,7 @@
 <div class="section-wrapper">
   <div class="section-inner">
     <div class="section-main sidebar-right">
-      <div class="section-content clearfix">
+      <div class="section-content">
         <aside class="sidebar-left">
 
           <!-- List of news -->
@@ -50,6 +50,7 @@
         </aside>
 
         <!-- The NEWS CONTENT itself -->
+        <div class="content-sidebar-left-wrapper">
         <div class="content-sidebar-left">
 
           <article class="news full">
@@ -106,12 +107,154 @@
 
             <div class="share-block">
               <h3>Порекомендовать друзьям</h3>
-
+              <div class="shares clearfix">
+                <a href="/" class="fb">251</a>
+                <a href="/" class="vk">22</a>
+                <a href="/" class="twitter">20</a>
+                <a href="/" class="ok">Odnoklassniki</a>
+                <a href="/" class="discuss">Обсудить <span>24</span></a>
+              </div>
             </div>
           </article>
 
+          <div class="read-more">
+            <h3>Читать на эту тему</h3>
+            <div class="read-more-list clearfix">
+              <article class="latest">
+                <div class="article-text">
+                  <div class="section-name">Столица</div>
+                  <h3 class="title"><a href="/">Неизвестные накрасили ногти на ногах Статуи Свободы</a></h3>
+                  <div class="summary">
+                    <p>Неизвестные накрасили ногти на ногах Статуи Свободы. Как рассказали сотрудники музея, ногти сатира раскрасили розовым лаком пришедшие в парк дети</p>
+                  </div>
+                  <div class="published">28 октября 2014</div>
+                </div>
+              </article>
+              <article class="latest">
+                <div class="article-text">
+                  <div class="section-name">Свинский рейтинг</div>
+                  <h3 class="title"><a href="/">ГПУ завершила расследование растраты 12 млн грн</a></h3>
+                  <div class="summary">
+                    <p>Генеральная прокуратура (ГПУ) завершила досудебное расследование в уголовном производстве по факту хищения государственных средств должностными лицами.</p>
+                  </div>
+                  <div class="published">28 октября 2014</div>
+                </div>
+              </article>
+            </div>
+          </div>
+
+        </div>
+        <div class="content-sidebar-left content-pager">
+          <div class="pager">
+            <div class="all">1-7 из 42</div>
+            <a href="/" class="prev">назад</a>
+            <a href="/" class="next">вперед</a>
+          </div>
+        </div>
+        </div>
+
+      </div>
+
+      <!-- BANNER section -->
+      <div class="banner b720">
+        <a href="/"><img src="http://dummyimage.com/720x90/dadada/ffffff.png" /></a>
+      </div>
+
+
+      <div class="related-content">
+        <h3>Другие материалы рубрики</h3>
+        <!-- THREE LATEST NEWS section -->
+        <div class="news-latest-3 clearfix">
+
+          <article class="latest">
+            <div class="article-image">
+              <a href="/"><img src="/content/image-mid4.jpg" width="240" height="120"/></a>
+            </div>
+            <div class="article-text">
+              <div class="section-name">Столица</div>
+              <h3 class="title"><a href="/">Неизвестные накрасили ногти на ногах Статуи Свободы</a></h3>
+              <div class="summary">
+                <p>Неизвестные накрасили ногти на ногах Статуи Свободы. Как рассказали сотрудники музея, ногти сатира раскрасили розовым лаком пришедшие в парк дети</p>
+              </div>
+              <div class="published">28 октября 2014</div>
+            </div>
+          </article>
+          <article class="latest">
+            <div class="article-image">
+              <a href="/"><img src="/content/image-mid5.jpg" width="240" height="120"/></a>
+            </div>
+            <div class="article-text">
+              <div class="section-name">Свинский рейтинг</div>
+              <h3 class="title"><a href="/">ГПУ завершила расследование растраты 12 млн грн</a></h3>
+              <div class="summary">
+                <p>Генеральная прокуратура (ГПУ) завершила досудебное расследование в уголовном производстве по факту хищения государственных средств должностными лицами.</p>
+              </div>
+              <div class="published">28 октября 2014</div>
+            </div>
+          </article>
+          <article class="latest">
+            <div class="article-image">
+              <a href="/"><img src="/content/image-mid6.jpg" width="240" height="120"/></a>
+            </div>
+            <div class="article-text">
+              <div class="section-name">Репортаж</div>
+              <h3 class="title"><a href="/">Украина потребует от сепаратистов свое</a></h3>
+              <div class="summary">
+                <p>Министерство энергетики и угольной промышленности планирует в ближайшее время осуществить поставки 2 млн т угля с подконтрольных сепаратистам территорий.</p>
+              </div>
+              <div class="published">28 октября 2014</div>
+            </div>
+          </article>
         </div>
       </div>
+
+      <div class="popular-content">
+        <h3>Популярные материалы</h3>
+        <!-- THREE LATEST NEWS section -->
+        <div class="news-latest-3 clearfix">
+
+          <article class="latest">
+            <div class="article-image">
+              <a href="/"><img src="/content/image-mid4.jpg" width="240" height="120"/></a>
+            </div>
+            <div class="article-text">
+              <div class="section-name">Столица</div>
+              <h3 class="title"><a href="/">Неизвестные накрасили ногти на ногах Статуи Свободы</a></h3>
+              <div class="summary">
+                <p>Неизвестные накрасили ногти на ногах Статуи Свободы. Как рассказали сотрудники музея, ногти сатира раскрасили розовым лаком пришедшие в парк дети</p>
+              </div>
+              <div class="published">28 октября 2014</div>
+            </div>
+          </article>
+          <article class="latest">
+            <div class="article-image">
+              <a href="/"><img src="/content/image-mid5.jpg" width="240" height="120"/></a>
+            </div>
+            <div class="article-text">
+              <div class="section-name">Свинский рейтинг</div>
+              <h3 class="title"><a href="/">ГПУ завершила расследование растраты 12 млн грн</a></h3>
+              <div class="summary">
+                <p>Генеральная прокуратура (ГПУ) завершила досудебное расследование в уголовном производстве по факту хищения государственных средств должностными лицами.</p>
+              </div>
+              <div class="published">28 октября 2014</div>
+            </div>
+          </article>
+          <article class="latest">
+            <div class="article-image">
+              <a href="/"><img src="/content/image-mid6.jpg" width="240" height="120"/></a>
+            </div>
+            <div class="article-text">
+              <div class="section-name">Репортаж</div>
+              <h3 class="title"><a href="/">Украина потребует от сепаратистов свое</a></h3>
+              <div class="summary">
+                <p>Министерство энергетики и угольной промышленности планирует в ближайшее время осуществить поставки 2 млн т угля с подконтрольных сепаратистам территорий.</p>
+              </div>
+              <div class="published">28 октября 2014</div>
+            </div>
+          </article>
+        </div>
+      </div>
+
     </div>
 
     <!-- SIDEBAR RIGHT -->
@@ -130,6 +273,86 @@
           <div class="text"><a href="/">«Третью революцию страна не переживет»</a></div>
         </article>
 
+        <div class="week-events-list">
+          <div class="events-logo">&nbsp;</div>
+          <h2 class="section-title">События недели</h2>
+
+          <!-- List of news -->
+          <div class="events-list">
+            <article class="event">
+              <div class="content">
+                <div class="date">16 сент</div>
+                <div class="section-name">Концерт</div>
+                <h3 class="title"><a href="/">Марчело Бурлон в Карабас Бар</a></h3>
+                <div class="venue">Центральный Дом Художника</div>
+              </div>
+            </article>
+            <article class="event">
+              <div class="content">
+                <div class="date">17 сент</div>
+                <div class="section-name">Концерт</div>
+                <h3 class="title"><a href="/">Грустный свингер: dj’s Dannyboy, Kush</a></h3>
+                <div class="venue">Concert Hall «Indigo», пр. Воздухофлотский 32</div>
+              </div>
+            </article>
+          </div>
+
+          <div class="more-link"><a href="/">Все события</a></div>
+        </div>
+
+
+        <!-- Quote -->
+        <article class="quoted clearfix">
+          <div class="image"><a href="/"><img src="/content/horosh.jpg" width="87" height="85" /></a></div>
+          <div class="name">Валерий Хорошковский</div>
+          <div class="text"><a href="/">«Третью революцию страна не переживет»</a></div>
+        </article>
+
+        <div class="popular-tags">
+          <h3>Популярные теги</h3>
+          <div class="tags-content taxonomy">
+            <a href="/">тег</a> <a href="/">ещё один</a> <a href="/">ха-ха</a>
+            <a href="/">няшно</a> <a href="/">фу</a> <a href="/">упс</a>
+            <a href="/">фу</a> <a href="/">свинский рейтинг</a>
+            <a href="/">тег</a> <a href="/">ещё один</a> <a href="/">ха-ха</a>
+            <a href="/">няшно</a> <a href="/">фу</a> <a href="/">упс</a>
+          </div>
+        </div>
+
+
+        <div class="front-news-list">
+          <div class="front-logo">&nbsp;</div>
+          <h2 class="section-title">Сейчас на главной</h2>
+          <div class="front-news-list-content">
+            <article class="latest">
+              <div class="article-image">
+                <a href="/"><img src="/content/image-mid2.jpg" width="240" height="120"/></a>
+              </div>
+              <div class="article-text">
+                <div class="section-name">Свинский рейтинг</div>
+                <h3 class="title"><a href="/">ГПУ завершила расследование растраты 12 млн грн</a></h3>
+              </div>
+            </article>
+            <article class="latest">
+              <div class="article-image">
+                <a href="/"><img src="/content/image-mid3.jpg" width="240" height="120"/></a>
+              </div>
+              <div class="article-text">
+                <div class="section-name">Репортаж</div>
+                <h3 class="title"><a href="/">Украина потребует от сепаратистов свое</a></h3>
+              </div>
+            </article>
+            <article class="latest">
+              <div class="article-image">
+                <a href="/"><img src="/content/image-mid2.jpg" width="240" height="120"/></a>
+              </div>
+              <div class="article-text">
+                <div class="section-name">Свинский рейтинг</div>
+                <h3 class="title"><a href="/">ГПУ завершила расследование растраты 12 млн грн</a></h3>
+              </div>
+            </article>
+          </div>
+        </div>
       </div>
     </aside>
   </div>

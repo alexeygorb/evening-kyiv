06b37c59666a0aeb1f6004a2539958f8c80c1a1d
Alexey Gorb <gorb.alexey@gmail.com>
2014-11-13 23:48:41 +0200

    Block scrolling.

--- layout/js/scripts.js
+++ layout/js/scripts.js
@@ -1,9 +1,32 @@
 $(function() {
 
+  equalizeBlocks();
+
+  $('.handbook-wrapper-inner').mCustomScrollbar();
+  $('.handbook-wrapper a').click(function(e) {
+    var link = $(this);
+    if (link.attr('href') == '#') {
+      e.preventDefault();
+      var item = link.parent();
+      var submenu = item.find('ul');
+      if (submenu.length > 0) {
+        if (submenu.hasClass('expanded')) {
+          submenu.slideUp(function() {submenu.removeClass('expanded');});
+        }
+        else {
+          submenu.slideDown(function() {submenu.addClass('expanded');});
+        }
+      }
+    }
+  });
+
+});
+
+var equalizeBlocks = function () {
+
   $('.news-latest-3,.news-latest-4').equalizer({columns: 'article'});
   $('.events-list').equalizer({columns: 'article'});
   $('.read-more-list').equalizer({columns: 'article'});
   $('.headlines .headline-row').equalizer({columns: '.headline'});
-  //$('.content-wrap:eq(1)').equalizer({ columns : '> div', max: 165, overflow : 'stinky' });
-  //$('.content-wrap:eq(2)').equalizer({ min: 140 });
-});
+
+};
--- layout/templates/page.tpl.php
+++ layout/templates/page.tpl.php
@@ -5,8 +5,10 @@
   <meta charset="UTF-8"/>
   <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,700italic,400,700&subset=cyrillic,cyrillic-ext,latin' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" type="text/css" href="/layout/css/style.css"/>
+  <link rel="stylesheet" type="text/css" href="/layout/css/jquery.mCustomScrollbar.css"/>
   <script type="text/javascript" src="/layout/js/jquery-1.11.1.min.js"></script>
   <script type="text/javascript" src="/layout/js/jquery.equalizer.min.js"></script>
+  <script type="text/javascript" src="/layout/js/jquery.mCustomScrollbar.concat.min.js"></script>
   <script type="text/javascript" src="/layout/js/scripts.js"></script>
 </head>
 <body class="<?php print $body_class; ?>">
